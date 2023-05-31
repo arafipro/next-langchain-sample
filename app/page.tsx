33540b7a28b1a29ba1369b38ff7b2160e1f33e82
Hiroki Chiyoda <contact@arafipro.com>
2023-05-31 21:45:55 +0900

    基本形が完成

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import { CharacterTextSplitter } from "langchain/text_splitter";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { CharacterTextSplitter } from "langchain/text_splitter";
+import { OpenAI } from "langchain/llms/openai";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { loadQAMapReduceChain } from "langchain/chains";
 
 export default async function Home() {
   const loader = new PDFLoader("data/pg.pdf", {
@@ -13,16 +17,24 @@ export default async function Home() {
     chunkOverlap: 24,
   });
   const output = await splitter.createDocuments([str]);
-
+  const model = new OpenAI({
+    openAIApiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  });
+  const embeddings = new OpenAIEmbeddings({
+    openAIApiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  });
+  const store = await MemoryVectorStore.fromDocuments(output, embeddings);
+  const question = "What is the name of your company?"; //会社名は何ですか？
+  const relevantDocs = await store.similaritySearch(question);
+  const chain = loadQAMapReduceChain(model);
+  const res = await chain.call({
+    input_documents: relevantDocs,
+    question,
+  });
   return (
     <main>
       <h1 className="text-center text-3xl">langchain</h1>
-      {output.map((obj, index) => (
-        <div key={index} className="p-2">
-          <p className="text-center text-2xl pb-2">Chank {index}</p>
-          <p>{obj.pageContent}</p>
-        </div>
-      ))}
+      <p className="text-center pt-2">{res.text}</p>
     </main>
   );
 }
